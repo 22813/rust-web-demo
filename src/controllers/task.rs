4f9d3cbae7229a410dd3875171830a7284a7e53b
hikelee <hikelee@qq.com>
2016-03-19 18:20:26 +0800

    refactor

--- src/controllers/task.rs
+++ src/controllers/task.rs
@@ -69,52 +69,15 @@ pub fn save(req: &mut Request) -> IronResult<Response> {
 
 
 
-
-
-
 /*
 pub struct HitCounter;
 impl Key for HitCounter{type Value=usize;}
-
-fn compose_response(data:&str)->IronResult<Response>{
-    let mut response = Response::new();
-    response.set_mut(status::Ok);
-    response.set_mut(data);
-    //let mut headers = Headers::new();
-    //headers.set( ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, Value::Utf8)])));
-    //response.headers=headers; 
-    response.headers.set(ContentType(Mime(TopLevel::Application,SubLevel::Json,vec![(Attr::Charset,Value::Utf8)])));
-    //response.headers.set(Cookie(vec![CookiePair::new("foo".to_owned(),"bar".to_owned())]));
-    Ok(response)
-}
-
-pub fn env(_: &mut Request) -> IronResult<Response> {
-    let powered_by:String = match env::var("POWERED_BY") {
-        Ok(val) => val,
-        Err(_) => "Iron".to_string()
-    };    
-    let message = format!("Powered by: {}, pretty cool aye", powered_by);
-    Ok(Response::with((status::Ok, message)))
-}
-
-
-
 pub fn hits(req: &mut Request) -> IronResult<Response> {
     let mutex = req.get::<Write<HitCounter>>().unwrap();
     let mut count = mutex.lock().unwrap();
     *count += 1;
     Ok(Response::with((status::Ok, format!("Hits: {}", *count))))
 } 
-fn get_json_data(req:&mut Request)->String{
-    let conn = req.get::<Read<AppDb>>().unwrap().get().unwrap();
-    let users=User::list_all(conn);
-    println!("users:{:?}",users);
-    println!("users.to_json():{:?}",users.to_json());
-    let mut data = BTreeMap::new();
-    data.insert("users".to_string(), users.to_json());
-    let data = json::encode(&data).unwrap();
-    data 
-}
 pub fn list_aes(req:&mut Request)->IronResult<Response>{
     let data =get_json_data(req);
     let data=crypto::aes_encrypt_string(&data);
@@ -125,10 +88,6 @@ pub fn list_aes(req:&mut Request)->IronResult<Response>{
 pub fn list_base64(req:&mut Request)->IronResult<Response>{
     let data =get_json_data( req);
     let data=crypto::base64_encode_string(&data).expect("");
-    compose_response(&data)
-}
-pub fn list(req:&mut Request)->IronResult<Response>{
-    let data =get_json_data( req);
     compose_response(&data)
 }
 */
--- src/utils/response.rs
+++ src/utils/response.rs
@@ -3,6 +3,8 @@ use iron::modifiers::Redirect;
 use iron::{Url, status};
 use rustc_serialize::json::{ToJson};
 use handlebars_iron::{Template};
+use hyper::header::{ContentType};
+use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 
 pub fn redirect(req:&Request,path:&str)->IronResult<Response>{
     let ref url=req.url;
@@ -13,5 +15,16 @@ pub fn template<T: ToJson>(name: &str, value: T) ->IronResult<Response>{
     let mut response = Response::new();
     response.set_mut(Template::new(name,value));
     response.set_mut(status::Ok);
+    Ok(response)
+}
+pub fn json_response(data:&str)->IronResult<Response>{
+    let mut response = Response::new();
+    response.set_mut(status::Ok);
+    response.set_mut(data);
+    //let mut headers = Headers::new();
+    //headers.set( ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, Value::Utf8)])));
+    //response.headers=headers; 
+    response.headers.set(ContentType(Mime(TopLevel::Application,SubLevel::Json,vec![(Attr::Charset,Value::Utf8)])));
+    //response.headers.set(Cookie(vec![CookiePair::new("foo".to_owned(),"bar".to_owned())]));
     Ok(response)
 }
