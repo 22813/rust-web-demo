379cd9211f0522ad20968a3e266b2687d0115f01
Admin <admin@ma.local>
2016-03-20 12:51:35 +0800

    add init script for table account

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -14,3 +14,20 @@ CREATE TABLE task
       update_time timestamp with time zone,
       status integer NOT NULL DEFAULT 0
 );
+
+CREATE SEQUENCE public.account_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 10000000
+  START 0
+  CACHE 1;
+
+
+CREATE TABLE account
+(
+      id integer NOT NULL DEFAULT nextval('account_seq'::regclass),
+      name character varying(30),
+      password character varying(100)
+);
+
+insert into account(name,password) values ('admin','admin');
