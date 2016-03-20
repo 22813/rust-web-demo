8e433bc5ac7e9c9101380acae8f7597f1f411256
Admin <admin@ma.local>
2016-03-20 12:27:32 +0800

    check login from database config

--- src/controllers/account.rs
+++ src/controllers/account.rs
@@ -1,34 +1,38 @@
 use iron::prelude::*;
+use std::collections::BTreeMap;
 use iron_login::User;
 use iron::modifiers::Redirect;
 use iron::{Url, status};
+use framework::database;
 
+use handlebars_iron::{Template};
 use utils::{response};
 use utils::request::*;
+use models::account::Account;
 #[derive(Debug)]
-struct Account(String);
-impl Account {
-    fn new(user_id: &str) -> Account {
-        Account(user_id.to_owned())
+struct MyUser(String);
+impl MyUser {
+    fn new(user_id: &str) -> MyUser {
+        MyUser(user_id.to_owned())
     }
 }
-impl User for Account {
-    fn from_user_id(_: &mut Request, user_id: &str) -> Option<Account> {
-        Some(Account(user_id.to_owned()))
+impl User for MyUser {
+    fn from_user_id(_: &mut Request, user_id: &str) -> Option<MyUser> {
+        Some(MyUser(user_id.to_owned()))
     }
     fn get_user_id(&self) -> &str {
         &self.0
     }
 }
 pub fn logout(req: &mut Request) -> IronResult<Response> {
-    let login = Account::get_login(req);
+    let login = MyUser::get_login(req);
     let ref url=req.url;
     let url = Url::parse(format!("{}://{}:{}/account/login/",url.scheme,url.host,url.port).as_str()).unwrap();
     Ok(Response::with((status::Found, Redirect(url.clone()))).set(login.log_out()))
 }
 
 pub fn check_login(req:&mut Request)->bool{
-    let login = Account::get_login(req);
+    let login = MyUser::get_login(req);
     let user=login.get_user();
     match user{
         None=>false,
@@ -37,19 +41,22 @@ pub fn check_login(req:&mut Request)->bool{
 }
 
 pub fn login(_: &mut Request) -> IronResult<Response> {
-    response::template("account-login","".to_owned())
+    let mut data = BTreeMap::new();
+    data.insert("error".to_string(),"".to_owned());
+    response::template("account-login",data)
 }
 
 pub fn do_login(req: &mut Request) -> IronResult<Response> {
-    let login = Account::get_login(req);
+    let login = MyUser::get_login(req);
     let name=req.get_form_param("name");
-    //let password=request::get_form_param(req,"password");
-    let ref url=req.url;
-    if let Some(name)=name{ 
-        //return Ok(Response::new() .set(::iron::status::Ok) .set(format!("User set to '{}'", name)).set(login.log_in(Account::new(&name))))
-        let url = Url::parse(format!("{}://{}:{}/task/",url.scheme,url.host,url.port).as_str()).unwrap();
-        let response=Response::with((status::Found, Redirect(url.clone()))).set(login.log_in(Account::new(&name)));
-        return Ok(response);
+    let password=req.get_form_param("password");
+    if let Some(account)=Account::get(database::get_conn(req),name,password) {
+        let mut response = Response::new().set(login.log_in(MyUser::new(&format!("{}",&account.id))));
+        response.set_mut(Template::new("index","".to_owned()));
+        response.set_mut(status::Ok);
+        return Ok(response)
     }
-    response::redirect(req,"/account/login/")
+    let mut data = BTreeMap::new();
+    data.insert("error".to_string(),"login name or password incorrect!".to_owned());
+    response::template("account-login",data)
 }
