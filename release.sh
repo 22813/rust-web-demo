f186136b5a805ce4719d4206dd8af39ac67c0a95
hikelee <hikelee@gmail.com>
2016-03-30 13:49:15 +0800

    add release method

---
+++ release.sh
@@ -0,0 +1,18 @@
+ERVER="8080"
+RUNTIME_DIR=~/$SERVER/runtime
+EXE=rust-web-demo-$SERVER
+cd ~/projects/rust-web-demo
+git pull
+
+cargo build --release
+
+ps aux | grep "$EXE" | grep -v grep| awk '{print [}'| xargs kill -9
+
+rm -Rf $RUNTIME_DIR
+mkdir $RUNTIME_DIR
+
+cp target/release/rust_web_demo $RUNTIME_DIR/$EXE
+cp -R web-root $RUNTIME_DIR/web-root
+
+cd ~/$SERVER/runtime
+nohup ./$EXE > ~/$SERVER/output.log 2>&1 &]
